9b5a3c08fdc9594c20b28aa91937bd0125dc3ce4
Sebastián Millán Ordóñez <sebastian.millanordonez2019@gmail.com>
2025-09-30 18:49:36 +0200

    Creación aplicación Angular

---
+++ luxurydelivery-frontend/src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    providePrimeNG({theme: {preset: Aura}})
+  ]
+};
